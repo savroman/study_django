5061b6abe1d16af84c0c68d1049c637d5320e1f5
Roman Savchuk <sav4ukrv@gmail.com>
2016-11-13 22:15:15 +0200

    add students photos base url

--- students/views/students.py
+++ students/views/students.py
@@ -2,26 +2,11 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Student
 
 # Views for Students
 def students_list(request):
-    students = (
-    {'id':1,
-     'first_name': u'Андрієнков',
-     'last_name': u'Андрій',
-     'ticket': 101,
-     'image': 'img/empty.png'},
-    {'id':2,
-     'first_name': u'Богданич',
-     'last_name': u'Богдан',
-     'ticket': 201,
-     'image': 'img/empty.png'},
-    {'id':3,
-     'first_name': u'Васильчук',
-     'last_name': u'Василь',
-     'ticket': 202,
-     'image': 'img/empty.png'}
-    )
+    students = Student.objects.all()
     return render(request, 'students/students_list.html', {'students': students})
 def students_add(request):
     return HttpResponse('<h1> Student add Form </h1>')
